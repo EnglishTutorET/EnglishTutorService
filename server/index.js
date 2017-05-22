34a94bb0efdb03b9fce0a14a2ca01cd628b6e168
Ranju <Ranju_Karmakar@external.mckinsey.com>
2017-05-22 10:26:53 +0530

    Ranju | refactored to change name of db handler to repository.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var middleware = require('./middleware');
+var repository = require('./repository');
 
 var port = 4444;
 
@@ -11,16 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/submit', function (req, res) {
     var firstName = req.body.firstname;
     var lastName = req.body.lastname;
-    middleware.insert(firstName, lastName);
+    repository.insert(firstName, lastName);
     res.redirect('/show');
 });
 
 app.get('/show', function (req, res) {
-    middleware.retrive().then(function (data) {
+    repository.retrive().then(function (data) {
         res.send(data)
     }).catch(err => {done(err)}
     )
 });
 
 app.listen(port);
-console.log("Listening on port", port);  
+console.log("Listening on port", port);
